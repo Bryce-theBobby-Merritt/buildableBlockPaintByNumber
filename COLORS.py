ca3ddb33941ec38877fe3be33faf374106ca3af0
Bryce Merritt <brycemerritt@mines.edu>
2023-12-07 13:17:21 -0700

    Added Some Color Names

---
+++ COLORS.py
@@ -0,0 +1,15 @@
+BRIGHT_PINK = None
+DARK_TURQOUISE = None
+TAN = None
+DARK_PINK = None
+MEDIUM_AZURE = None
+MEDIUM_STONE_GRAY = None
+DARK_BLUE = None
+YELLOW = None
+LIME = None
+WHITE = None
+LIGHT_AQUA = None
+BLACK = None
+CORAL = None
+
+#TODO - Web scrape all color names from brickowl.com
